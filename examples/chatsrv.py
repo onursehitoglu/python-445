512ffb1fff47e044e8365433b7ad360772d95af4
Onur Tolga Sehitoglu <onur@localhost.localdomain>
2024-11-22 15:00:42 +0300

    chat server update

--- examples/chatsrv.py
+++ examples/chatsrv.py
@@ -1,105 +1,105 @@
 #!/usr/bin/python
-from websockets.sync import server
-from websockets.exceptions	import ConnectionClosedError,ConnectionClosedOK
-
-from threading import Thread,Lock,Condition
+from threading import Thread,RLock,Condition
 import socket
-import json
 import sys
 
-class Chat:
+class Monitor:
+	'''A generic monitor class, derive from this class and
+	   call super().__init__()
+	   then decorate sync methods with Monitor.sync '''
 	def __init__(self):
+		self.mlock = RLock()
+	@classmethod
+	def sync(self, method):
+		def w(self, *p, **kw):
+			with self.mlock:
+				return method(self,*p, **kw)
+		return w
+
+	def CV(self):
+		'''Create condition variables with this method to get
+		   them share the monitor lock'''
+		return Condition(self.mlock)
+
+class Chat(Monitor):
+	def __init__(self):
+		super().__init__()
 		self.buf = []
-		self.lock = Lock()
-		self.newmess = Condition(self.lock)
+		self.newmess = self.CV()
+
+	@Monitor.sync
 	def newmessage(self,mess):
-		self.lock.acquire()
 		self.buf.append(mess)
 		self.newmess.notify_all()
-		self.lock.release()
+
+	@Monitor.sync
 	def getmessages(self,after=0):
-		self.lock.acquire()
 		if len(self.buf) < after:
 			a = []
 		else:
 			a = self.buf[after:]
-		self.lock.release()
 		return a
-		
-class NotifierAgent(Thread):
-	def __init__(self, sock, chat):
-		self.sock = sock
+
+	@Monitor.sync
+	def wait(self):
+		self.newmess.wait()
+
+class WRAgent(Thread):
+	def __init__(self, sock, addr, chat):
+		self.sock, self.addr = sock, addr
 		self.chat = chat
 		self.current = 0
 		self.notexit = True
 		Thread.__init__(self)
 	def run(self):
-		oldmess = self.chat.getmessages()
-		self.current += len(oldmess)
-		self.sock.send(json.dumps(oldmess))
-		while self.notexit:
-			self.chat.lock.acquire()
-			self.chat.newmess.wait()
-			self.chat.lock.release()
+		while True:
 			oldmess = self.chat.getmessages(self.current)
-			if len(oldmess) == 0:
-				continue
-			self.current += len(oldmess)
-			try:
-			  self.sock.send(json.dumps(oldmess))
-			except:
-			  self.notexit = False
-	def terminate():
-		self.notexit = False
-		self.chat.lock.acquire()
-		self.newmess.notify_all()
-		self.chat.lock.release()
-			
+			if len(oldmess) != 0:
+				self.current += len(oldmess)
+				try:
+					mlist = [str(addr) + ":" + m.strip().decode() 
+									for (addr,m) in oldmess] + ['']
+					self.sock.send('\n'.join(mlist).encode())
+				except Exception:
+					print('Writer terminating')
+					break
+
+			self.chat.wait()
+
+class RDAgent(Thread):
+	def __init__(self, sock, addr, chatroom):
+		self.sock, self.addr, self.chatroom = sock, addr, chatroom
+		super().__init__()
+
+	def run(self):
+		while True:
+			inp = self.sock.recv(1024)
+			if not inp:
+				break
+			self.chatroom.newmessage((self.addr,inp) )
+		print('peer closed the connection')
+		self.sock.close()
+		self.chatroom.newmessage( (self.addr,b'bye') )
 
 if len(sys.argv) != 2:
 	print('usage: ',sys.argv[0],'port')
 	sys.exit(-1)
 
-
-HOST = ''         
+HOST = ''		 
 PORT = int(sys.argv[1] )
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((HOST, PORT))
-#s.listen(1)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
 
 chatroom = Chat()
 
-def Agent(sock):
-	global chatroom
-
-	rdr = NotifierAgent(sock,chatroom)
-	rdr.start() 
-	
-	try:
-		while True:
-			inp = sock.recv()
-			mess = json.loads(inp)
-			print(mess)
-			if 'message' in mess: chatroom.newmessage(mess['message'])
-	except ConnectionClosedOK:
-		print('client is terminating')
-	except ConnectionClosedOK:
-		print('client generated error')
-
-	sock.close()
-	rdr.terminate()
 
 
-srv =  server.serve(Agent, host=HOST, port=PORT) 
-print(dir(srv))
-srv.serve_forever()
-#while True:
-#	conn, addr = s.accept()
-#
-#	print('Connected by', addr)
-#	a = RDAgent(conn,addr,chatroom)
-#	b = WRAgent(conn,addr,chatroom)
-#	a.start()
-#	b.start()
+while True:
+	conn, addr = s.accept()
 
-
+	print('Connected by', addr)
+	a = RDAgent(conn,addr,chatroom)
+	b = WRAgent(conn,addr,chatroom)
+	a.start()
+	b.start()
